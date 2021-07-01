744d80765980b4be2fa46d1d8d6dcc10a1180fd7
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2021-07-01 23:44:32 +0200

    added middleware checker for requests

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -6,39 +6,77 @@ export const router = express.Router();
 // parse post body content so that it can be accessed - https://stackoverflow.com/questions/5710358/how-to-access-post-form-fields
 router.use(express.json());
 
-router.get("/", (req,res) => {
-    res.sendFile("/public/app/app.html",{ root: __dirname + "/.." });
+router.get("/", (req, res) => {
+    res.sendFile("/public/app/app.html", { root: __dirname + "/.." });
 });
 
-router.get("/payments", (req,res) => {
-    getFinanceEntries((financeEntries : any) => {
+router.get("/payments", (req, res) => {
+    getFinanceEntries((financeEntries: any) => {
         res.send(financeEntries);
     });
 });
 
-router.post("/create", async (req,res) => {
+router.post("/create" , checkCreateRequest);
+router.post("/create", async (req, res) => {
     await insertFinanceEntry(req.body);
-    getFinanceEntries((financeEntries : any) => {
+    getFinanceEntries((financeEntries: any) => {
         res.send(financeEntries);
     });
 });
 
-router.post("/update", async (req,res) => {
-    // console.log(req.body);
+router.post("/udpate" , checkUpdateRequest);
+router.post("/update", async (req, res) => {
     await replaceFinanceEntry(
-        {name : req.body.originalName},
+        { name: req.body.originalName },
         req.body.editedPayment
     );
-    getFinanceEntries((financeEntries : any) => {
+    getFinanceEntries((financeEntries: any) => {
         res.send(financeEntries);
     });
 })
 
-router.post("/delete", async (req,res) => {
+
+router.post("/delete", checkDeleteRequest);
+router.post("/delete", async (req, res) => {
     await deleteFinanceEntry({
-        name : req.body.name
+        name: req.body.name
     });
-    getFinanceEntries((financeEntries : any) => {
+    getFinanceEntries((financeEntries: any) => {
         res.send(financeEntries);
     });
 });
+
+
+
+function checkDeleteRequest(req : any,res : any,next : any){
+    if (req.body.name != null){
+        return next();
+    }
+    console.log("Request not possible due to invalid data format");
+    getFinanceEntries((financeEntries : any) => {
+        res.send(financeEntries);
+    });
+}
+
+function checkCreateRequest(req : any, res : any, next : any){
+    if(
+        req.body.name != null &&
+        req.body.organization != null &&
+        req.body.amount != null &&
+        req.body.selectedMonth != null &&
+        req.body.rythm != null &&
+        req.body.type != null
+    ){
+        return next();
+    }
+    console.log("Request not possible due to invalid data format");
+    getFinanceEntries((financeEntries : any) => {
+        res.send(financeEntries);
+    });
+}
+
+function checkUpdateRequest(req : any, res : any, next : any){
+    checkCreateRequest(req,res,next);
+}
+
+
